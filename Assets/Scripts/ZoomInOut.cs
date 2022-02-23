cdfc7aba7dc0da503bd11b8f6c69de062fbe754e
saranghein <saranghein@gmail.com>
2022-02-23 15:13:21 +0900

    zoomin정도 조정

--- Assets/Scripts/ZoomInOut.cs
+++ Assets/Scripts/ZoomInOut.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class ZoomInOut : MonoBehaviour
 {
+    float fieldOfView = 60f;
     public float orthoZoomSpeed = 0.5f;
     private void Update()
     {
-        if (Input.touchCount == 2)
+        if (Input.touchCount == 2&&(Input.touches[0].phase == TouchPhase.Moved || Input.touches[1].phase == TouchPhase.Moved))
         {
             Touch touchOne = Input.GetTouch(0);//첫번째 손가락 좌표
             Touch touchTwo = Input.GetTouch(1);//두번째 손가락 좌표
@@ -17,12 +18,15 @@ public class ZoomInOut : MonoBehaviour
 
             float preTouchDelta = (touchOnePrevPos - touchTwoPrevPos).magnitude;// 움직임의 크기
             float touchDelta = (touchOne.position - touchTwo.position).magnitude;//현재 위치 크기
-
             float deltaMagDiff = preTouchDelta - touchDelta;//얼마큼 확대, 축소 할지
-            
+
+ //           fieldOfView -= (touchDelta - preTouchDelta) * 0.01f;
+
             GetComponent<Camera>().orthographicSize += deltaMagDiff * orthoZoomSpeed;//줌인, 아웃
-            GetComponent<Camera>().orthographicSize = Mathf.Max(GetComponent<Camera>().orthographicSize, 0.1f);//사이즈가 0아래로 안떨어지도록함
-            
+            GetComponent<Camera>().orthographicSize = Mathf.Clamp(GetComponent<Camera>().orthographicSize, 1f,5f);//사이즈가 0아래로 안떨어지도록함
+//            GetComponent<Camera>().orthographicSize = Mathf.Lerp(GetComponent<Camera>().orthographicSize, fieldOfView, Time.deltaTime * 4);
+ //           preTouchDelta = touchDelta;
+
         }
     }
 }
